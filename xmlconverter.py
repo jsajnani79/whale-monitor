55a35755a789f99c6c74a615aab657f2e7289dbc
jsajnani79 <jsajnani79@gmail.com>
2015-05-13 23:36:54 -0700

    updated xmlToCSV script, added support to combine csvs

--- xmlconverter.py
+++ xmlconverter.py
@@ -7,16 +7,31 @@ import sys
 import csv
 import xml.etree.ElementTree as ET
 
+import glob
+import os
+import time
+from itertools import izip_longest
+import fnmatch
+import datetime
+from datetime import timedelta, datetime
+import shutil
+import time
+from sys import argv
+
+import pandas as pd
+
 def xmlToCsv(inFile, outFile):
 	NS = 'http://www.sec.gov/edgar/document/thirteenf/informationtable'
 	header = ('name', 'class', 'cusip', 'value', 'shares')
 
 	tree = ET.parse(inFile)
 	root = tree.getroot()
-
+	fileName = inFile[9:-4]
+	filingDate = fileName[-10:]
+	fundName = fileName[:-11]
 	with open(outFile, 'w') as f:
 	    writer = csv.writer(f)
-	    writer.writerow(header)
+	    # writer.writerow(header)
 	    for infoTable in root.iter('{%s}infoTable' % NS):
 	        name = infoTable[0].text
 	        classNm = infoTable[1].text
@@ -26,9 +41,16 @@ def xmlToCsv(inFile, outFile):
 	        real_value = int(value) * 1000
 	        shares = infoTable[4][0].text
 
-	        row = name, classNm, cusip, real_value, shares
+	        row = fundName, filingDate, name, classNm, cusip, real_value, shares
 	        writer.writerow(row)
 
+def mergeCSVs(readDir, outFile):
+	fout=open(outFile,"a")
+	allFiles = glob.glob(readDir + "/*.csv")
+	for file in allFiles:
+	    for line in open(file):
+	         fout.write(line)    
+	fout.close()
 
 
 
